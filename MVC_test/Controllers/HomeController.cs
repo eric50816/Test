994f6135cb8857891b44f3d2f1da9a5070404756
BANK\119719 <eeric50816@gmail.com>
2020-12-15 10:41:36 +0800

    加入專案檔案。

---
+++ MVC_test/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_test.Models;
+
+namespace MVC_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DateTime date = DateTime.Now;
+            ViewBag.Date = date;
+
+            Student data = new Student("1", "小明", 80);
+            return View(data);
+        }
+    }
+}
